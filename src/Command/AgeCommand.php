436e12a4c3988cd2c913cc84a7e3de42be2ad7f1
Evaldas Vaitonis <ritmas@Evaldass-iMac.local>
2019-05-03 21:33:19 +0300

    age calculation

--- src/Command/AgeCommand.php
+++ src/Command/AgeCommand.php
@@ -9,11 +9,13 @@
 namespace App\Command;
 
 
+use DateTime;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 
 class AgeCommand extends Command
@@ -25,7 +27,7 @@ class AgeCommand extends Command
         $this
             // the short description shown while running "php bin/console list"
             ->setDescription('Calculates your age by given date of birth')
-            ->addArgument('date of birth', InputArgument::OPTIONAL,'yyyy-mm-dd')
+            ->addArgument('dob', InputArgument::OPTIONAL,'Date of birth yyyy-mm-dd')
             ->setHelp('This command allows you to calculate age by given date of birth')
             ->addOption('--adult', 'a', InputOption::VALUE_NONE, 'Tells if you are over 18 years old' )
         ;
@@ -33,6 +35,14 @@ class AgeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Whoa!');
+        $io = new SymfonyStyle($input,$output);
+        $dob = $input->getArgument('dob');
+
+        if($dob) {
+            $bday = new DateTime($dob);
+            $today = new Datetime(date('m.d.y'));
+            $age = $today->diff($bday);
+            $io->note(sprintf('I am %s years old',$age->y));
+        }
     }
 }
