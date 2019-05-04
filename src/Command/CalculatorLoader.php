58e9638ddec8ab0a5dcfe4f885fc9d9e2e0afd5b
Evaldas Vaitonis <ritmas@Evaldass-iMac.local>
2019-05-04 17:53:51 +0300

    calculators are now used as services

--- src/Command/CalculatorLoader.php
+++ src/Command/CalculatorLoader.php
@@ -8,28 +8,47 @@
 
 namespace App\Command;
 
-
-use App\Calculator\AdultCalculator;
-use App\Calculator\AgeCalculator;
+use App\Calculator\CalculatorInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
 class CalculatorLoader
 {
-
+    /** @var CalculatorInterface[] */
+    private $calculator;
 
     public function info(InputInterface $input, OutputInterface $output)
     {
 
         if($input->getOption('adult')){
-            $loader = new AdultCalculator();
-            $loader->printInfo($input,$output);
+            $loader = $this->getCalculator('age');
         } else {
-            $loader = new AgeCalculator();
-            $loader->printInfo($input,$output);
+            $loader = $this->getCalculator('adult');
         }
 
+        $loader->printInfo($input,$output);
+    }
+
+    /**
+     * @param $calculatorName
+     * @return CalculatorInterface
+     */
+    public function getCalculator($calculatorName): CalculatorInterface
+    {
+        if (!isset($this->calculator[$calculatorName])) {
+            throw new \RuntimeException('Provider with name: "' . $calculatorName . '" does not exist');
+        }
+        return $this->calculator[$calculatorName];
+    }
+
+    /**
+     * @param $calculatorName
+     * @param CalculatorInterface $calculatorService
+     */
+    public function addCalculator($calculatorName, CalculatorInterface $calculatorService): void
+    {
+        $this->calculator[$calculatorName] = $calculatorService;
     }
     
 }
